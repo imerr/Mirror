90e82298d759b3eee45691331493bf40fae34544
vis2k <info@noobtuts.com>
2021-07-08 22:03:06 +0800

    bufferSizeLimit: smaller default

--- Assets/Mirror/Components/NetworkTransform2k/NetworkTransformBase.cs
+++ Assets/Mirror/Components/NetworkTransform2k/NetworkTransformBase.cs
@@ -66,7 +66,7 @@ namespace Mirror
         public int bufferTimeMultiplier = 3;
         public float bufferTime => sendInterval * bufferTimeMultiplier;
         [Tooltip("Buffer size limit to avoid ever growing list memory consumption attacks.")]
-        public int bufferSizeLimit = 100;
+        public int bufferSizeLimit = 64;
 
         [Tooltip("Start to accelerate interpolation if buffer size is >= threshold. Needs to be larger than bufferTimeMultiplier.")]
         public int catchupThreshold = 6;
